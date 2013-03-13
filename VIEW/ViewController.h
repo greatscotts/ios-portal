11f96f2935d958b93f8f716e30fc6b1f5752ba18
greatscotts <javelonagian@live.com>
2013-03-13 22:52:40 +0800

    Initial Commit

---
+++ VIEW/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VIEW
+//
+//  Created by Gian Scottie Javelona on 3/13/13.
+//  Copyright (c) 2013 Gian Scottie Javelona. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ VIEW/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VIEW
+//
+//  Created by Gian Scottie Javelona on 3/13/13.
+//  Copyright (c) 2013 Gian Scottie Javelona. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
